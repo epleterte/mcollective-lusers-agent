e1d2ac3ccec5587e98fa5f8b118bc60399f435e5
Christian Bryn <chr.bryn@gmail.com>
2013-08-05 23:07:19 +0200

    handle new actions (poorly)

--- application/lusers.rb
+++ application/lusers.rb
@@ -26,22 +26,33 @@ EOF
 
     def main
       # XXX: action is currently ignored
-      #action = configuration[:action]
+      action = configuration[:action]
 
       mc = rpcclient('lusers', :options => options)
 
-      mc.who() do |r|
-        begin
-            lusers = []
-            r[:body][:data][:msg].each do |l|
-              lusers << l['user']
-            end
-            unless lusers.empty?
-              printf("%-40s: %s\n", r[:senderid], lusers.join(','))
-            end
-        rescue RPCError => e
-            puts "The RPC agent returned an error: #{e}"
+      case action
+      when "who"
+        mc.who() do |r|
+          begin
+              lusers = []
+              r[:body][:data][:msg].each do |l|
+                lusers << l['user']
+              end
+              unless lusers.empty?
+                printf("%-40s: %s\n", r[:senderid], lusers.join(','))
+              end
+          rescue RPCError => e
+              puts "The RPC agent returned an error: #{e}"
+          end
         end
+      when "wall"
+        # XXX: sanitize .. ? probably 8)
+        mc.wall(:msg => ARGV)
+      when "has_user"
+        # XXX: probably give some output here...
+        mc.has_user(:user => ARGV )
+      when "has_group"
+        mc.has_group(:group => ARGV )
       end
 
       printrpcstats
